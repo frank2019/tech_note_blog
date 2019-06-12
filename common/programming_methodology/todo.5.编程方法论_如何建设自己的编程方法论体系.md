fae08be5f78f3947df5b41ca4bd06d4b531286a5
frank <454052811@qq.com>
2019-06-12 08:30:58 +0800

    info.py  add git pull ops

---
+++ common/programming_methodology/todo.5.编程方法论_如何建设自己的编程方法论体系.md
@@ -0,0 +1,21 @@
+
+
+
+
+```
+用对了方法，事情就解决了一半，编程领域尤其如此！
+```
+
+
+
+
+
+书单：  
+
+1. 《程序设计实践》 英文版    https://book.douban.com/subject/2111800/
+
+2. https://pymlovelyq.github.io/posts/e140888d/
+
+3. https://blog.csdn.net/qq_43560338/article/details/85209654
+
+4. # 斯坦福大学开放课程：编程方法论
--- scripts/info.py
+++ scripts/info.py
@@ -1,6 +1,39 @@
 import datetime
+import os
 
-weekth = datetime.datetime.now().isocalendar()
-print(weekth)
-str = input("Press any key to quit: ");
-#print ("%s", str)
+def get_day_info():
+    weekth = datetime.datetime.now().isocalendar()
+    print(weekth)
+    #str = input("Press any key to quit: ");
+    #print ("%s", str)
+
+def execCmd(cmd):  
+    r = os.popen(cmd)  
+    text = r.read()  
+    r.close()  
+    return text  
+
+
+def do_git_pull():
+    repo_dir_home = 'F:\\workspace\\tech_note_blog'
+    repo_dir_work = 'F:\\workspace\\tech_note_blog'
+
+    if os.path.exists(repo_dir_home):
+        repo_dir =  repo_dir_home
+    elif os.path.exists(repo_dir_work):
+        repo_dir = repo_dir_work
+    else :
+        print('repo dir do net exist')
+        return 
+
+    
+    cmd_git_status='git -C ' + repo_dir + ' status'
+    cmd_git_pull = 'git -C ' + repo_dir + ' pull'
+    #os.system("git  -C F:\\workspace\\tech_note_blog status")
+    print(cmd_git_pull)
+    ret = execCmd(cmd_git_pull)
+    print(ret)
+
+if __name__ == '__main__':
+    get_day_info()
+    do_git_pull()
